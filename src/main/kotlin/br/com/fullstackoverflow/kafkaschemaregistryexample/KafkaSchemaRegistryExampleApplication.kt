cb3dbe425c9609a3d2766c136e007ed04547771b
Roger Pimenta <roger.pimenta@live.com>
2022-05-25 17:30:45 -0300

    Config producer

--- src/main/kotlin/br/com/fullstackoverflow/kafkaschemaregistryexample/KafkaSchemaRegistryExampleApplication.kt
+++ src/main/kotlin/br/com/fullstackoverflow/kafkaschemaregistryexample/KafkaSchemaRegistryExampleApplication.kt
@@ -1,10 +1,21 @@
 package br.com.fullstackoverflow.kafkaschemaregistryexample
 
+import br.com.fullstackoverflow.kafkaschemaregistryexample.entity.Pessoa
+import br.com.fullstackoverflow.kafkaschemaregistryexample.producer.PessoaProducerImpl
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KafkaSchemaRegistryExampleApplication
+class KafkaSchemaRegistryExampleApplication(
+	val pessoaProducerImpl: PessoaProducerImpl
+): ApplicationRunner {
+	override fun run(args: ApplicationArguments?) {
+		val pessoa = Pessoa("José", "Antunes")
+		pessoaProducerImpl.persist("12345", pessoa)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<KafkaSchemaRegistryExampleApplication>(*args)
